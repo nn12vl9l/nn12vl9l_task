2101eb26004f0aa7324d397fca55d9f46880445d
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-09-20 21:00:17 +0900

    マイグレーションとシーダー作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks' ,[App\Http\Controllers\TaskControllers::class, 'index']);
