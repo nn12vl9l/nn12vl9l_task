88b8bfb3cbb6a1bb2376c77c0452329556d6c5b8
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-09-20 21:13:15 +0900

    一旦

--- app/Http/Controllers/TaskControlles.php
+++ app/Http/Controllers/TaskControlles.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskControlles extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index' ,['tasks' => $tasks]);
+    }
+
 }
